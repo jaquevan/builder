715569e2a6eb2a1a339fc7ef6d8ae40b4d11ab7a
jaquevan <jaquevan@bu.edu>
2025-03-24 11:59:02 -0400

    responsive contact page

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -111,7 +111,7 @@ export default function NavBar() {
         { href: "/about", label: "About" },
         { href: "/experience", label: "Experience" },
         { href: "/projects", label: "Projects" },
-        { href: "/contact", label: "Contact" },
+        // { href: "/contact", label: "Contact" },
     ];
 
     return (
